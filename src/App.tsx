feacca6ff6c1cc2f287135ae93909766c1de6345
colinmarley <colinmarley94@gmail.com>
2023-07-22 11:21:45 -0700

    Adds Avatar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
+
 import './App.css';
+import CLAvatar from './component/common/CLAvatar';
 
 function App() {
+  console.log("App Starting")
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CLAvatar fullName={''}/>
     </div>
   );
 }
---
+++ src/component/common/CLAvatar.tsx
@@ -0,0 +1,49 @@
+import { Avatar } from '@mui/material';
+import { deepOrange, green } from '@mui/material/colors';
+import React from 'react';
+
+interface ICLAvatarProps {
+    fullName: string
+}
+
+function CLAvatar(props: ICLAvatarProps) {
+    
+
+    function stringToColor(string: string) {
+        let hash = 0;
+        let i;
+      
+        /* eslint-disable no-bitwise */
+        for (i = 0; i < string.length; i += 1) {
+          hash = string.charCodeAt(i) + ((hash << 5) - hash);
+        }
+      
+        let color = '#';
+      
+        for (i = 0; i < 3; i += 1) {
+          const value = (hash >> (i * 8)) & 0xff;
+          color += `00${value.toString(16)}`.slice(-2);
+        }
+        /* eslint-enable no-bitwise */
+      
+        return color;
+    }
+      
+    function stringAvatar(name: string) {
+        return {
+          sx: {
+            bgcolor: stringToColor(name),
+          },
+          children: `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`,
+        };
+    }
+
+    return(
+        <div className="cl-button">
+            <h1>Avatar</h1>
+            <Avatar {...stringAvatar(props.fullName)}></Avatar>
+        </div>
+    )
+}
+
+export default CLAvatar;
